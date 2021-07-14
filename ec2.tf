5860bce32ce843cf1ff967942b3b34cc449ae4e8
Nageswara Rao <nageshraoj@gmail.com>
2021-07-14 17:18:05 +0530

    updated count

--- ec2.tf
+++ ec2.tf
@@ -14,6 +14,6 @@ resource "aws_instance" "demoec2" {
         sudo echo this is from $(hostname)>/var/www/html/index.html
   EOF
   tags = {
-    "Name" = "Demo EC2 ${index+1}"
+    "Name" = "Demo EC2 ${count.index+1}"
   }
 }
